6a2c21ae8c0e6189bb86aed23a5101772c9840a4
Diego Aguilef <diego.aguilef@gmail.com>
2018-11-22 12:50:18 -0300

    init commit

---
+++ src/fruteria/ui/app/FruteriaUiApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fruteria.ui.app;
+
+/**
+ *
+ * @author kdieg
+ */
+public class FruteriaUiApp {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
